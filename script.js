5b82baea0155eb1b299a8e7b7e7f5dde12f2c89e
fayz <fayzgame38@gmail.com>
2023-06-24 02:19:42 +0700

    menambahkan jam

--- script.js
+++ script.js
@@ -2,46 +2,50 @@
 document.addEventListener("scroll", function () {
   const navbar = document.querySelector(".navbar");
   if (scrollY > 0) {
-    navbar.classList.add("navbar-dark", "bg-dark", "shadow-sm");
+    navbar.classList.add("navbar-dark", "bg-dark", "shadow");
   } else {
-    navbar.classList.remove("navbar-dark", "bg-dark", "shadow-sm");
+    navbar.classList.remove("navbar-dark", "bg-dark", "shadow");
   }
 });
+
 //CALENDAR
-//clock
-const clock = document.getElementById("clock");
-//date
-const time = new Date();
-const date = document.getElementById("date");
-//day
-const day = document.getElementById("day");
-const Day = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-//month
-const month = document.getElementById("month");
-const Month = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "June",
-  "July",
-  "Aug",
-  "Sept",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-day.innerHTML = `${Day[time.getDay()]}`;
-date.innerHTML = `${time.getDate()}`;
-month.innerHTML = `${Month[time.getMonth()]}`;
-clock.innerHTML = `-${time.getTime()}-`;
-console.log(time);
+function Calendar() {
+  //clock
+  const clock = document.getElementById("clock");
+  //date
+  const time = new Date();
+  const date = document.getElementById("date");
+  //day
+  const day = document.getElementById("day");
+  const Day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  //month
+  const month = document.getElementById("month");
+  const Month = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  day.innerHTML = `${Day[time.getDay()]}`;
+  date.innerHTML = `${time.getDate()}`;
+  month.innerHTML = `${Month[time.getMonth()]}`;
+  clock.innerHTML = `-${time.getHours()}:${time.getMinutes()}-`;
+  window.requestAnimationFrame(Calendar);
+}
+Calendar();
